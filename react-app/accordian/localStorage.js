9220e741714da4f61a09c3f76006db05a4611b7e
uihyunkimm <dmlxld@gmail.com>
2024-04-13 22:59:41 +0900

    UPDATE accordian list

---
+++ react-app/accordian/localStorage.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    const panelFaqContainer = document.querySelectorAll('.panel-faq-container');
+    const panelFaqAnswer = document.querySelectorAll('.panel-faq-answer');
+    
+    const btnRLD = document.getElementById('btnRemoveLocalData');
+    
+    btnRLD.addEventListener('click',() => {
+        
+    })
+}
